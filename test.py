82104c75fb1a2ed78f933540ef275179ce461904
fortable1999 <fortable1999@gmail.com>
2013-06-20 23:47:07 +0900

    ...

---
+++ test.py
@@ -0,0 +1,4 @@
+import os
+
+p =  os.path.abspath(__file__)
+print os.path.dirname(p)
--- zmblog/settings.py
+++ zmblog/settings.py
@@ -1,6 +1,6 @@
 # Django settings for zmblog project.
 import os
-PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -68,8 +68,10 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-	os.path.join(PROJECT_ROOT, '/static/'),
+	os.path.join(PROJECT_ROOT, 'static/'),
 )
+print PROJECT_ROOT
+print STATICFILES_DIRS
 
 # List of finder classes that know how to find static files in
 # various locations.
